74600a64a20dd7dad99fd80ffb7c3916e12a64a2
Brian Cardarella <bcardarella@gmail.com>
2010-05-28 22:37:12 -0400

    Basic jquery validate plugin support - required - format

--- javascript/lib/jquery.client_side_validations.js
+++ javascript/lib/jquery.client_side_validations.js
@@ -1,2 +1,34 @@
-
-// Your library here
+function jQueryValidateAdapter(object, validations) {
+  rules    = {}
+  messages = {}
+  for(var attr in validations) {
+    name = object + '[' + attr + ']';
+    rules[name]    = {};
+    messages[name] = {};
+    for(var validation in validations[attr]) {
+      rule = null;
+      switch(validation) {
+        case 'presence':
+          rule  = 'required'
+          value = true;
+          break;
+        case 'format':
+          value = validations[attr][validation]['with'];
+          break;
+        default:
+      }
+      if(rule == null) {
+        rule = validation;
+      }
+      rules[name][rule]    = value;
+      messages[name][rule] = validations[attr][validation]['message'];
+    }
+  }
+  
+  result = {
+    rules : rules,
+    messages : messages
+  };
+  
+  return result;
+}
--- javascript/spec/unit/jquery.client_side_validations.spec.js
+++ javascript/spec/unit/jquery.client_side_validations.spec.js
@@ -1,8 +1,43 @@
-
-describe 'YourLib'
-  describe '.someMethod()'
-    it 'should do something'
-      true.should.be true
+describe 'jQuery Client Side Validations'
+  describe 'adapters'
+    describe 'jquery.validate'
+      describe 'required'
+        before
+          validations = {
+            "number": { 
+              "presence": { "message":"can't be blank" }
+            }
+          }
+          result = jQueryValidateAdapter('object', validations);
+        end
+      
+        it 'should translate the rule'
+          result.rules['object[number]']['required'].should.be_true
+        end
+        
+        it 'should translate the message'
+          result.messages['object[number]']['required'].should.equal "can't be blank"
+        end
+      end
+      
+      describe 'format'
+        before
+          validations = {
+            "number": {
+              "format": { "message":"is invalid", "with":/\d/ }
+            }
+          }
+          result = jQueryValidateAdapter('object', validations)
+        end
+        
+        it 'should translate the rule'
+          result.rules['object[number]']['format'].should.eql(/\d/)
+        end
+        
+        it 'should translate the message'
+          result.messages['object[number]']['format'].should.equal "is invalid"
+        end
+      end
     end
   end
 end
