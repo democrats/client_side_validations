d2f86d456f825cff411084d50c68cea2567d9ff2
Brian Cardarella <bcardarella@gmail.com>
2010-07-06 00:03:24 -0400

    Client Side Validations middleware

--- lib/client_side_validations.rb
+++ lib/client_side_validations.rb
@@ -1,4 +1,44 @@
 require 'rubygems' unless defined?(Gem)
+require 'json'
+
+class ClientSideValidations
+  def initialize(app)
+    @app    = app
+  end
+  
+  def call(env)
+    case env['PATH_INFO']
+    when %r{/(\w+)/validations.json}
+      resource = $1
+      body = get_validations(resource)
+      [200, {'Content-Type' => 'application/json', 'Content-Length' => "#{body.length}"}, body]
+    when %r{/(\w+)/validations/uniqueness/(\w+)}
+      resource  = $1
+      attribute = $2
+      value     = env['QUERY_STRING'].split('=').last
+      body = {"unique" => is_unique?(resource, attribute, value) }.to_json
+      
+      [200, {'Content-Type' => 'application/json', 'Content-Length' => "#{body.length}"}, body]
+    else
+      @app.call(env)
+    end
+  end
+  
+  private
+  
+  def get_validations(resource)
+    constantize_resource(resource).new.validations_to_json
+  end
+  
+  def is_unique?(resource, attribute, value)
+    constantize_resource(resource).send("find_by_#{attribute}", value) == nil
+  end
+  
+  def constantize_resource(resource)
+    eval(resource.split('_').map{|word| word.capitalize}.join)
+  end
+  
+end
 
 module DNCLabs
   module ClientSideValidations
@@ -17,9 +57,7 @@ module DNCLabs
   end
 end
 
-
 # ORM
-
 if defined?(ActiveModel)
   require 'adapters/active_model'
   unless Object.respond_to?(:to_json)
@@ -35,13 +73,13 @@ elsif defined?(ActiveRecord)
   end
 end
 
-klass.class_eval do
-  include DNCLabs::ClientSideValidations
+if klass
+  klass.class_eval do
+    include DNCLabs::ClientSideValidations
+  end
 end
 
-
 # Template
-
 if defined?(ActionView)
   require 'adapters/action_view'
 end
---
+++ spec/middleware_spec.rb
@@ -0,0 +1,48 @@
+require 'client_side_validations'
+require 'spec_helper'
+require 'rack/test'
+require 'crack/json'
+
+describe 'Client Side Validations Middleware' do
+  include Rack::Test::Methods
+  
+  def app
+    app = Proc.new {|env| [200,{},"successfully hit rails app"]}
+    ClientSideValidations.new(app)
+  end
+  
+  context 'multiple resources' do
+    before do
+      class Book; end
+      class PartnerUser; end
+    end
+    
+    describe 'rules' do
+      it 'should generate a rules path for the book resource' do
+        Book.any_instance.stubs(:validations_to_json).returns("VALIDATIONS_TO_JSON")
+        get '/book/validations.json'
+        last_response.body.should == "VALIDATIONS_TO_JSON"
+      end
+    
+      it 'should generate a rules path for the partner_user resource' do
+        PartnerUser.any_instance.stubs(:validations_to_json).returns("VALIDATIONS_TO_JSON")
+        get '/partner_user/validations.json'
+        last_response.body.should == "VALIDATIONS_TO_JSON"
+      end
+    end
+    
+    describe 'uniqueness' do
+      it 'should generate a uniqueness path for the book resource' do
+        Book.stubs(:find_by_name).returns(nil)
+        get '/book/validations/uniqueness/name', { :value => 'Test' }
+        Crack::JSON.parse(last_response.body).should == {"unique" => true}
+      end
+
+      it 'should generate a uniqueness path for the partner_user resource' do
+        PartnerUser.stubs(:find_by_name).returns("Found a record")
+        get '/partner_user/validations/uniqueness/name', { :value => 'Test' }
+        Crack::JSON.parse(last_response.body).should == {"unique" => false}
+      end
+    end
+  end
+end
